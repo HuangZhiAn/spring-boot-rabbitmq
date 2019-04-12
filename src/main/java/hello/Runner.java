8f8d12a65ba0ad5f33cbbd0292d4eb83470bd79e
黄志安 <zhian.huang@hand-china.com>
2019-04-12 17:31:16 +0800

    设置回调

--- src/main/java/hello/Runner.java
+++ src/main/java/hello/Runner.java
@@ -25,6 +25,15 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Sending message..."+new SimpleDateFormat("yyyy-MM-dd"));
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            System.out.println("回调函数，当前线程id：" + Thread.currentThread());
+            //自己的处理逻辑
+            System.out.println("confirm--:correlationData:" + correlationData + ",ack:" + ack + ",cause:" + cause + "message" + "messageVo");
+        });
+        rabbitTemplate.setReturnCallback((msg, replyCode, replyText, exchange2, routingKey2) -> {
+            //路由失败回调
+
+        });
         rabbitTemplate.convertAndSend(Application.QUEUE_NAME, "Hello from RabbitMQ!");
         receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
         context.close();
